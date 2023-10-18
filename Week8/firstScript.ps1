7c5d43558a279a9c43e9935c59c742c1dee3e817
Max Gerew <offbrandmax@gmail.com>
2023-10-18 13:17:47 -0400

    Completed Week 8 Assignments

---
+++ Week8/firstScript.ps1
@@ -0,0 +1,9 @@
+﻿cd C:\Users\max\Desktop
+
+clear
+
+Get-EventLog -list
+
+$readlog = Read-host -Prompt "Please select a log to review"
+
+Get-EventLog -LogName $readlog -Newest 3 | export-csv -NoTypeInformation -Path "C:\Users\max\Desktop\rep.csv"
